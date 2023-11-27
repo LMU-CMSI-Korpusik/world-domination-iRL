f2d3b6bbab814b7ebd052b93550a76d55f8d4f32
Kieran Ahn <kahn3@lion.lmu.edu>
2023-11-27 08:34:19 -0800

    RandomPlayer now uses numpy random number generation because python doesn't like it when I say randint with the endpoints the same

--- src/players.py
+++ src/players.py
@@ -7,7 +7,9 @@ Date: 11/27/2023
 
 from riskGame import Card, Territory
 from riskLogic import Board, Player, Rules
-import random
+import numpy as np
+
+rng = np.random.default_rng()
 
 
 class RandomPlayer(Player):
@@ -16,13 +18,13 @@ class RandomPlayer(Player):
     """
 
     def get_claim(self, board: Board, free_territories: set[Territory]) -> Territory:
-        return random.choice(list(free_territories))
+        return rng.choice(list(free_territories))
 
     def place_armies(self, board: Board, armies_to_place: int) -> tuple[Territory, int]:
-        return (random.choice(list(self.territories)), random.randint(1, armies_to_place))
+        return (rng.choice(list(self.territories)), rng.integers(1, armies_to_place, endpoint=True))
 
     def attack(self, board: Board) -> tuple[Territory, Territory, int]:
-        stop_attack = random.random() * (self.occupied_territories() + 1) < 1
+        stop_attack = rng.random() * (self.occupied_territories() + 1) < 1
         occupied_territories = set(list(self.territories))
 
         if stop_attack:
@@ -38,7 +40,7 @@ class RandomPlayer(Player):
             if len(valid_attack_targets) == 0:
                 return (None, None, None)
 
-            target = random.choice(list(valid_attack_targets))
+            target = rng.choice(list(valid_attack_targets))
             valid_attack_targets.remove(target)
 
             valid_bases = self.get_valid_bases(
@@ -47,20 +49,21 @@ class RandomPlayer(Player):
             if len(valid_bases) != 0:
                 choosing = False
 
-        base = random.choice(list(valid_bases))
+        base = rng.choice(list(valid_bases))
 
-        attacking_armies = random.randint(1, min(board.armies[base], 3))
+        attacking_armies = rng.integers(
+            1, min(board.armies[base], 3, endpoint=True))
 
         return target, base, attacking_armies
 
     def capture(self, board: Board, target: Territory, base: Territory, attacking_armies: int) -> int:
-        return random.randint(attacking_armies, board.armies[base] - 1)
+        return rng.integers(attacking_armies, board.armies[base])
 
     def defend(self, board_state: Board, target: Territory) -> int:
-        return random.randint(1, min(board_state.armies[target], 2))
+        return rng.integers(1, min(board_state.armies[target], 2), endpoint=True)
 
     def fortify(self, board: Board) -> tuple[Territory, Territory, int]:
-        no_fortify = random.random() * (self.occupied_territories() + 1) < 1
+        no_fortify = rng.random() * (self.occupied_territories() + 1) < 1
 
         if no_fortify:
             return (None, None, None)
@@ -76,7 +79,7 @@ class RandomPlayer(Player):
             if len(possible_destinations) == 0:
                 return (None, None, None)
 
-            destination = random.choice(possible_destinations)
+            destination = rng.choice(possible_destinations)
             possible_destinations.remove(destination)
 
             valid_sources = self.get_valid_bases(
@@ -85,12 +88,12 @@ class RandomPlayer(Player):
             if len(valid_sources) != 0:
                 choosing = False
 
-        source = random.choice(list(valid_sources))
+        source = rng.choice(list(valid_sources))
 
-        return destination, source, random.randint(1, board.armies[source] - 1)
+        return destination, source, rng.integers(1, board.armies[source])
 
     def use_cards(self, board: Board) -> tuple[Card, Card, Card]:
-        return random.choice(Rules.get_matching_cards(self.hand))
+        return rng.choice(Rules.get_matching_cards(self.hand))
 
     def choose_extra_deployment(self, board: Board, potential_territories: list[Territory]) -> Territory:
-        return random.choice(potential_territories)
+        return rng.choice(potential_territories)
