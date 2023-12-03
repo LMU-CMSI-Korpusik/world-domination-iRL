6a5e9c9c9acb837625ba0c02a1606e0d904be0d6
Kieran Ahn <kieranahn@gmail.com>
2023-12-03 06:27:10 -0800

    moved Rules to top of file because it was bothering me having to scroll past it every single time while debugging

--- src/riskLogic.py
+++ src/riskLogic.py
@@ -15,6 +15,124 @@ class Board:
     pass
 
 
+class Rules:
+    """
+    The rules for running a game of Risk
+    """
+
+    @staticmethod
+    def get_matching_cards(cards: list[Card]) -> list[tuple[Card, Card, Card]]:
+        """
+        Finds all the valid matches in a hand of cards
+
+        :params:\n
+        cards   --  A list of Cards
+
+        :returns:\n
+        matches --  A list of tuples containing matched Cards
+        """
+        matches = list()
+
+        for i in range(len(cards)):
+            for j in range(i, len(cards)):
+                if i == j:
+                    continue
+                # Yes, having a triple for loop hurts my eyes, too.
+                for k in range(j, len(cards)):
+                    if j == k:
+                        continue
+                    if (cards[i].design == cards[j].design and cards[j].design == cards[k].design) or (cards[i].design != cards[j].design and cards[j].design != cards[k].design and cards[k].design != cards[i].design) or any([cards[i].wildcard, cards[j].wildcard, cards[k].wildcard]):
+                        # Don't talk to me about this conditional. -Kieran
+                        matches.append((cards[i], cards[j], cards[k]))
+
+        return matches
+
+    @staticmethod
+    def get_initial_armies(n_players: int) -> int:
+        """
+        Gets the initial number of armies a player starts the game with.
+
+        :params:\n
+        n_players   --  the number of players in the game
+
+        :returns:\n
+        armies      --  the initial armies for each player
+        """
+        if n_players < 1:
+            raise ValueError(
+                f'Please tell me how playing Risk with {n_players} players is mathematically possible.'
+            )
+        if n_players == 1:
+            raise ValueError("You cannot play Risk by yourself.")
+        if n_players == 2:
+            raise NotImplementedError(
+                "2 player Risk has not been implemented yet.")
+        if n_players > 6:
+            raise ValueError("Risk does not support more than six players.")
+        else:
+            return 35 - (5 * (n_players - 3))
+
+    @staticmethod
+    def resolve_attack(attacker_rolls: list[int], defender_rolls: list[int]) -> tuple[int, int]:
+        """
+        Determines whether the attacker or defender wins when attacking a
+        Territory
+
+        :params:\n
+        attacker_rolls  --  The dice rolls the attacker made\n
+        defender_rolls  --  The dice rolls the defender made
+
+        :returns:\n
+        attacker_losses --  the number of armies the attacker lost\n
+        defender_losses --  the number of armies the defender lost
+        """
+        attacker_rolls.sort()
+        defender_rolls.sort()
+        attacker_losses = 0
+        defender_losses = 0
+
+        while len(attacker_rolls) > 0 and len(defender_rolls) > 0:
+            highest_attacker_roll = attacker_rolls.pop()
+            highest_defender_roll = defender_rolls.pop()
+            if highest_attacker_roll > highest_defender_roll:
+                defender_losses += 1
+            else:
+                attacker_losses += 1
+
+        return attacker_losses, defender_losses
+
+    @staticmethod
+    def get_armies_from_card_match(matches_made: int) -> int:
+        """
+        The amount of armies turning in a matched set of cards will get you
+
+        :params:\n
+        matches_made    --  the number of matched trios turned in so far
+
+        :returns:\n
+        armies          --  the amount of armies awarded
+        """
+        if matches_made < 5:
+            return 4 + 2 * matches_made
+        elif matches_made == 5:
+            return 15
+        else:
+            return (matches_made - 2) * 5
+
+    @staticmethod
+    def get_armies_from_territories_occupied(occupied_territories: int) -> int:
+        """
+        The amount of armies awarded from occupying territories
+
+        :params:\n
+        occupied_territories    --  the number of territories a player occupies
+
+        :returns:\n
+        armies                  --  the amount of armies awarded
+        """
+        return max(3, occupied_territories // 3)
+
+
 @dataclass
 class Player:
     """
@@ -281,124 +399,6 @@ class Board:
             self.return_and_shuffle(*player_cards)
             player.remove_cards(*player_cards)
         self.matches_traded = 0
-
-
-class Rules:
-    """
-    The rules for running a game of Risk
-    """
-
-    @staticmethod
-    def get_matching_cards(cards: list[Card]) -> list[tuple[Card, Card, Card]]:
-        """
-        Finds all the valid matches in a hand of cards
-
-        :params:\n
-        cards   --  A list of Cards
-
-        :returns:\n
-        matches --  A list of tuples containing matched Cards
-        """
-        matches = list()
-
-        for i in range(len(cards)):
-            for j in range(i, len(cards)):
-                if i == j:
-                    continue
-                # Yes, having a triple for loop hurts my eyes, too.
-                for k in range(j, len(cards)):
-                    if j == k:
-                        continue
-                    if (cards[i].design == cards[j].design and cards[j].design == cards[k].design) or (cards[i].design != cards[j].design and cards[j].design != cards[k].design and cards[k].design != cards[i].design) or any([cards[i].wildcard, cards[j].wildcard, cards[k].wildcard]):
-                        # Don't talk to me about this conditional. -Kieran
-                        matches.append((cards[i], cards[j], cards[k]))
-
-        return matches
-
-    @staticmethod
-    def get_initial_armies(n_players: int) -> int:
-        """
-        Gets the initial number of armies a player starts the game with.
-
-        :params:\n
-        n_players   --  the number of players in the game
-
-        :returns:\n
-        armies      --  the initial armies for each player
-        """
-        if n_players < 1:
-            raise ValueError(
-                f'Please tell me how playing Risk with {n_players} players is mathematically possible.'
-            )
-        if n_players == 1:
-            raise ValueError("You cannot play Risk by yourself.")
-        if n_players == 2:
-            raise NotImplementedError(
-                "2 player Risk has not been implemented yet.")
-        if n_players > 6:
-            raise ValueError("Risk does not support more than six players.")
-        else:
-            return 35 - (5 * (n_players - 3))
-
-    @staticmethod
-    def resolve_attack(attacker_rolls: list[int], defender_rolls: list[int]) -> tuple[int, int]:
-        """
-        Determines whether the attacker or defender wins when attacking a
-        Territory
-
-        :params:\n
-        attacker_rolls  --  The dice rolls the attacker made\n
-        defender_rolls  --  The dice rolls the defender made
-
-        :returns:\n
-        attacker_losses --  the number of armies the attacker lost\n
-        defender_losses --  the number of armies the defender lost
-        """
-        attacker_rolls.sort()
-        defender_rolls.sort()
-        attacker_losses = 0
-        defender_losses = 0
-
-        while len(attacker_rolls) > 0 and len(defender_rolls) > 0:
-            highest_attacker_roll = attacker_rolls.pop()
-            highest_defender_roll = defender_rolls.pop()
-            if highest_attacker_roll > highest_defender_roll:
-                defender_losses += 1
-            else:
-                attacker_losses += 1
-
-        return attacker_losses, defender_losses
-
-    @staticmethod
-    def get_armies_from_card_match(matches_made: int) -> int:
-        """
-        The amount of armies turning in a matched set of cards will get you
-
-        :params:\n
-        matches_made    --  the number of matched trios turned in so far
-
-        :returns:\n
-        armies          --  the amount of armies awarded
-        """
-        if matches_made < 5:
-            return 4 + 2 * matches_made
-        elif matches_made == 5:
-            return 15
-        else:
-            return (matches_made - 2) * 5
-
-    @staticmethod
-    def get_armies_from_territories_occupied(occupied_territories: int) -> int:
-        """
-        The amount of armies awarded from occupying territories
-
-        :params:\n
-        occupied_territories    --  the number of territories a player occupies
-
-        :returns:\n
-        armies                  --  the amount of armies awarded
-        """
-        return max(3, occupied_territories // 3)
 
 
 class Risk:
