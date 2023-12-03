d7c6fe3c28302d461ef2a9d2d7c9be4cf6941fca
Kieran Ahn <kieranahn@gmail.com>
2023-12-03 06:11:47 -0800

    Fixed imports

--- src/testGame.py
+++ src/testGame.py
@@ -2,14 +2,15 @@
 A demonstration of the Risk game working using RandomPlayers
 
                                         ########## IMPORTANT ##########
-BEFORE RUNNING THIS, DISABLE THE INCREMENT OF self.board.matches_traded IN Risk.play()!!!!!
+BEFORE RUNNING THIS, DISABLE THE INCREMENT OF self.board.matches_traded IN Risk.tradein()!!!!!
 RandomPlayers are garbage and the game will go on forever if you don't.                                    
 
 Author: Kieran Ahn
 Date: 11/27/2023
 """
 
-from riskLogic import Risk, ClassicBoard, Rules
+from riskLogic import Risk, Rules
+from boards import ClassicBoard
 from players import RandomPlayer
 
 players = list()
