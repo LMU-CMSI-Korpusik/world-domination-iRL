11f9b48b8af2355dd10b9e78fbe74eab1be67978
Kieran Ahn <kieranahn@gmail.com>
2023-12-03 17:25:37 -0800

    added test game to test basic model-based players

---
+++ src/testGameSmart.py
@@ -0,0 +1,33 @@
+"""
+A demonstration Risk game using RiskPlayers
+
+Author: Kieran Ahn
+Date: 12/3/2023
+"""
+
+from riskLogic import Risk, Rules
+from boards import ClassicBoard
+from players import RiskPlayer
+from riskNet import RiskNet
+from riskGame import Action
+
+net = RiskNet()
+
+players = list()
+players.append(RiskPlayer('GLaDOS', net))
+players.append(RiskPlayer('Skynet', net))
+players.append(RiskPlayer('HAL', net))
+
+board = ClassicBoard(players)
+game = Risk(players, Rules(), board)
+game.play(quiet=False)
+print(f'\n\nstate of {players[0].name}:')
+print(board.get_state_for_player(players[0], Action.CLAIM))
+
+print(f'\n\nstate of {players[1].name}:')
+print(board.get_state_for_player(players[1], Action.CLAIM))
+
+print(f'\n\nstate of {players[2].name}:')
+print(board.get_state_for_player(players[2], Action.CLAIM))
+
+board.reset()
